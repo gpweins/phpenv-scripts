530bc8a75b1aa528eed7c1b8b15ff7c9b79e48e4
Gustavo Weinschütz <gpweins@gmail.com>
2019-09-15 18:42:58 -0300

    Fix enable different PHP versions to be defined with different aliases

--- scripts/installers/php.sh
+++ scripts/installers/php.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
 DIR=`dirname $0`
-PHP_HOME=~/.config/php-cli
+
+PHP_ALIAS=${1:-'php'}
+PHP_TAG=${2:-'latest'}
+PHPUNIT_ALIAS=${3:-'phpunit'}
+
+PHP_HOME="$HOME/.config/php-cli-$PHP_TAG"
 
 if [ ! -d "$PHP_HOME" ]; then
     mkdir "$PHP_HOME"
 fi
 
-docker pull php:7.3-cli-alpine
+docker pull "php:$PHP_TAG"
 
 echo "Copying docker-php script to $PHP_HOME/"
-cp "$DIR/../runners/docker-php.sh" "$PHP_HOME/docker-php"
-chmod u+x "$PHP_HOME/docker-php"
+cp "$DIR/../runners/docker-php.sh" "$PHP_HOME/docker-php-$PHP_TAG"
+sed -i "s|PHP_TAG|$PHP_TAG|g" "$PHP_HOME/docker-php-$PHP_TAG"
+chmod u+x "$PHP_HOME/docker-php-$PHP_TAG"
 
-echo "Checking for php alias..."
+echo "Checking for $PHP_ALIAS alias..."
 if [ ! -f ~/.bash_aliases ]; then
     touch ~/.bash_aliases
 fi
-grep -q "alias php" ~/.bash_aliases
+grep -q "alias $PHP_ALIAS" ~/.bash_aliases
 missing_alias=$?
 
 if [ $missing_alias = 1 ]; then
-    echo "PHP alias don't exist, adding it!"
+    echo "Alias $PHP_ALIAS don't exist, adding it!"
     if [ -s ~/.bash_aliases ]; then
         echo >> ~/.bash_aliases
     fi
-    printf "alias php=$PHP_HOME/docker-php" >> ~/.bash_aliases
+    printf "alias $PHP_ALIAS=$PHP_HOME/docker-php-$PHP_TAG" >> ~/.bash_aliases
 else
-    echo "PHP alias already exist!"
+    echo "Alias $PHP_ALIAS already exist!"
 fi
 
-echo "Checking for phpunit alias..."
-grep -q "alias phpunit" ~/.bash_aliases
+echo "Checking for $PHPUNIT_ALIAS alias..."
+grep -q "alias $PHPUNIT_ALIAS" ~/.bash_aliases
 missing_alias=$?
 
 if [ $missing_alias = 1 ]; then
-    echo "PHPUnit alias don't exist, adding it!"
+    echo "Alias $PHPUNIT_ALIAS don't exist, adding it!"
     if [ -s ~/.bash_aliases ]; then
         echo >> ~/.bash_aliases
     fi
-    printf "alias phpunit='$PHP_HOME/docker-php vendor/phpunit/phpunit/phpunit'" >> ~/.bash_aliases
+    printf "alias $PHPUNIT_ALIAS='$PHP_HOME/docker-php-$PHP_TAG vendor/phpunit/phpunit/phpunit'" >> ~/.bash_aliases
 else
-    echo "PHPUnit alias already exist!"
+    echo "Alias $PHPUNIT_ALIAS already exist!"
 fi
--- scripts/runners/docker-php.sh
+++ scripts/runners/docker-php.sh
@@ -2,5 +2,5 @@ docker run -it --rm \
   --volume $PWD:/usr/src/app \
   --workdir /usr/src/app \
   --user $(id -u):$(id -g) \
-  php:7.3-cli-alpine \
+  php:PHP_TAG \
   php "$@"
