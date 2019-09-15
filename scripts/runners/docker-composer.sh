c6e55dc9026c9ba766e5f16dfedf2310c87affc3
Gustavo Weinschütz <gpweins@gmail.com>
2019-09-15 13:28:05 -0300

    Fix issue with cache

--- scripts/runners/docker-composer.sh
+++ scripts/runners/docker-composer.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 COMPOSER_HOME=$HOME/.config/composer
-COMPOSER_CACHE_DIR=$HOME/.cache/composer
+COMPOSER_HOME_LOCAL=$COMPOSER_HOME-COMPOSER_TAG
+COMPOSER_CACHE_DIR=$HOME/.cache/composer-COMPOSER_TAG
 
 docker run -it --rm \
   --env COMPOSER_HOME \
   --env COMPOSER_CACHE_DIR \
-  --volume $COMPOSER_HOME:$COMPOSER_HOME \
-  --volume $COMPOSER_CACHE_DIR:$COMPOSER_CACHE_DIR \
+  --volume $COMPOSER_HOME_LOCAL:$COMPOSER_HOME \
+  --volume $COMPOSER_CACHE_DIR:/.composer/cache/ \
   --volume $PWD:/app \
   --user $(id -u):$(id -g) \
   composer:COMPOSER_TAG "$@"
