1c27b4eb6a4696252fbecc8516dc0f70e117488d
Gustavo Weinschütz <gpweins@gmail.com>
2019-09-25 02:48:18 -0300

    Update PHP structure to use the container that is running

--- scripts/installers/php.sh
+++ scripts/installers/php.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-DIR=`dirname $0`
-
-PHP_ALIAS=${1:-'php'}
-PHP_TAG=${2:-'latest'}
-PHPUNIT_ALIAS=${3:-'phpunit'}
-
-PHP_HOME="$HOME/.config/php-cli-$PHP_TAG"
-
-if [ ! -d "$PHP_HOME" ]; then
-    mkdir "$PHP_HOME"
-fi
-
-docker pull "php:$PHP_TAG"
-
-echo "Copying docker-php script to $PHP_HOME/"
-cp "$DIR/../runners/docker-php.sh" "$PHP_HOME/docker-php-$PHP_TAG"
-sed -i "s|PHP_TAG|$PHP_TAG|g" "$PHP_HOME/docker-php-$PHP_TAG"
-chmod u+x "$PHP_HOME/docker-php-$PHP_TAG"
+PHP_ALIAS='php'
+PHPUNIT_ALIAS='phpunit'
 
 echo "Checking for $PHP_ALIAS alias..."
 if [ ! -f ~/.bash_aliases ]; then
@@ -31,7 +15,7 @@ if [ $missing_alias = 1 ]; then
     if [ -s ~/.bash_aliases ]; then
         echo >> ~/.bash_aliases
     fi
-    printf "alias $PHP_ALIAS=$PHP_HOME/docker-php-$PHP_TAG" >> ~/.bash_aliases
+    printf "alias $PHP_ALIAS='docker exec -it \$(docker ps -f \"name=php\" --format \"{{.Names}}\") /usr/local/bin/php'" >> ~/.bash_aliases
 else
     echo "Alias $PHP_ALIAS already exist!"
 fi
@@ -45,7 +29,7 @@ if [ $missing_alias = 1 ]; then
     if [ -s ~/.bash_aliases ]; then
         echo >> ~/.bash_aliases
     fi
-    printf "alias $PHPUNIT_ALIAS='$PHP_HOME/docker-php-$PHP_TAG vendor/phpunit/phpunit/phpunit'" >> ~/.bash_aliases
+    printf "alias $PHPUNIT_ALIAS='docker exec -it \$(docker ps -f \"name=php\" --format \"{{.Names}}\") /usr/local/bin/php ./vendor/bin/phpunit'" >> ~/.bash_aliases
 else
     echo "Alias $PHPUNIT_ALIAS already exist!"
 fi
--- scripts/runners/docker-php.sh
+++
@@ -1,6 +0,0 @@
-docker run -it --rm \
-  --volume $PWD:/usr/src/app \
-  --workdir /usr/src/app \
-  --user $(id -u):$(id -g) \
-  php:PHP_TAG \
-  php "$@"
